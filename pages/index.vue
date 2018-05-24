6a0ed2484b9a78ee6a6b607838a0fa1d6829a71e
pakorn <kptpp57@gmail.com>
2018-05-24 12:07:01 +0700

    12.00

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,56 @@
 <template>
-  <v-conrainer>
-    <v-layout column>
-        <v-flex>
-             <h1>Register</h1>
-        </v-flex>
-        <v-flex>
-            <v-text-field v-model="user" label="ชื่อผู้ใช้"/>
-        </v-flex>
-   
-        <v-flex>
-            <v-text-field v-model="pass" type="password" label="รหัสผ่าน"/>   
-        </v-flex>
-    
-        <v-flex>
-            <v-text-field v-model="pass2" type="password" label="รหัสผ่านอีกครั้ง"/>
-        </v-flex>
-    
-        <v-flex>
-            <v-btn @click="register"><v-icon>favorite</v-icon> "ลงทะเบียน" </v-btn> 
-        </v-flex>
-    
-    </v-layout>
-  </v-conrainer>
+  <div>
+    <h1>Student List</h1>
+    <v-select
+      :items="clsList"
+      v-model="cls"
+      label="Select"
+      single-line
+    />
+    <select v-model="cls">
+      <option value="1">1</option>
+      <option value="2">2</option>
+    </select>
+    <ul>
+      <li v-for="st in students" :key="st.id">
+        {{ st.code }} {{ st.firstName }} {{ st.lastName }}
+        <v-btn @click="editStudent(st.id)">Edit</v-btn>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      user: '',
-      pass: '',
-      pass2: '',
+      cls: '1',
+      students: [],
+      clsList: [
+        { value: '1', text: 'ชั้นปีที่ 1' },
+        { value: '2', text: 'ชั้นปีที่ 2' },
+        { value: '3', text: 'ชั้นปีที่ 3' },
+      ],
     }
   },
-  methods: {
-    register() {
-
+  watch: {
+    cls() {
+      this.getStudent()
     },
   },
+  async created() {
+    console.log('created')
+    this.getStudent()
+  },
+  methods: {
+    async getStudent() {
+      // let res = await this.$http.get('/student?class=' + this.cls)
+      let res = await this.$http.get('/student', {params: {class: this.cls}})
+      this.students = res.data.student
+    },
+    editStudent(id) {
+      this.$router.push('/student/edit?id=' + id)
+    },
+  }, // methods
 }
 </script>
+
