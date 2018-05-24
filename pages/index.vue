fbc48911cbd7fcb81f3cdaa64d715481e38ae6c5
pakorn <kptpp57@gmail.com>
2018-05-24 09:49:13 +0700

    1

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>Hello world</div>
+  <v-conrainer>
+    <v-layout column>
+        <v-flex>
+             <h1>Register</h1>
+        </v-flex>
+        <v-flex>
+            <v-text-field v-model="user" label="ชื่อผู้ใช้"/>
+        </v-flex>
+   
+        <v-flex>
+            <v-text-field v-model="pass" type="password" label="รหัสผ่าน"/>   
+        </v-flex>
+    
+        <v-flex>
+            <v-text-field v-model="pass2" type="password" label="รหัสผ่านอีกครั้ง"/>
+        </v-flex>
+    
+        <v-flex>
+            <v-btn @click="register"><v-icon>favorite</v-icon> "ลงทะเบียน" </v-btn> 
+        </v-flex>
+    
+    </v-layout>
+  </v-conrainer>
 </template>
 <script>
 export default {
   data() {
     return {
-      id: 1,
+      user: '',
+      pass: '',
+      pass2: '',
     }
+  },
+  methods: {
+    register() {
+
+    },
   },
 }
 </script>
