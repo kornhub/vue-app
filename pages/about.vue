a918284a3c19fa259ded93a37f03617c2a061eb6
pakorn <kptpp57@gmail.com>
2018-05-24 11:28:21 +0700

    store1

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>About <i>{{ name.toUpperCase() }}</i></h1>
+    <h2>age <b>{{ age * 2 }}</b></h2>
+    <input :value="name" type="text"><br>
+    <input v-model="name" type="text">
+    <input v-model="name" type="text">
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'pakorn',
+      age: 25,
+    }
+  },
+}
+</script>
+
